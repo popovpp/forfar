eb74ed21bced5e149debc196baeee27a05b88f1e
popovpp <popov.pavel@220-volt.ru>
2022-05-26 02:11:24 +0300

    Added wkhtmltopdf

--- forfar/forfar/settings.py
+++ forfar/forfar/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e50!r6r6me*uh01p&das%p6qs=()p1jlo!&ge3oy9b(c(r+rx#'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- forfar/receipt/models.py
+++ forfar/receipt/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Printer(models.Model):
+
+    name = models.CharField(max_length=255, verbose_name='Наименование принтера')
+    api_key = models.CharField(max_length=32, verbose_name='Ключ доступа к API')
+    check_type = models.CharField(max_length=16, verbose_name='Тип чека')
+    point_id = models.IntegerField(verbose_name='Идентификатор точки')
