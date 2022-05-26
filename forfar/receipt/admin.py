db06950e687c503c78c9656571f04c0953eb3b68
popovpp <popov.pavel@220-volt.ru>
2022-05-26 17:30:45 +0300

    Admin pages was done

--- forfar/receipt/admin.py
+++ forfar/receipt/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Printer, Check
+
+
+class PrinterModelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'api_key', 'check_type', 'point_id']
+    list_display_links = ['id']
+    list_filter = ['name', 'api_key', 'check_type', 'point_id']
+    search_fields = ['name', 'api_key', 'check_type', 'point_id']
+
+    class Meta:
+        model = Printer
+
+
+class CheckModelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'printer_id', 'type', 'order', 'status', 'pdf_file']
+    list_display_links = ['id']
+    list_filter = ['printer_id', 'type', 'order', 'status', 'pdf_file']
+    search_fields = ['printer_id', 'type', 'order', 'status', 'pdf_file']
+
+    class Meta:
+        model = Check
+
+
+admin.site.register(Printer, PrinterModelAdmin)
+admin.site.register(Check, CheckModelAdmin)
--- forfar/receipt/models.py
+++ forfar/receipt/models.py
@@ -5,5 +5,44 @@ class Printer(models.Model):
 
     name = models.CharField(max_length=255, verbose_name='Наименование принтера')
     api_key = models.CharField(max_length=32, verbose_name='Ключ доступа к API')
-    check_type = models.CharField(max_length=16, verbose_name='Тип чека')
+    check_type = models.CharField(max_length=10, verbose_name='Тип чека')
     point_id = models.IntegerField(verbose_name='Идентификатор точки')
+
+    def __unicode__(self):
+        return self.name
+
+    def __str__(self):
+        return self.name
+
+    class Meta():
+        verbose_name = 'Принтер'
+        verbose_name_plural = 'Принтеры'
+        db_table = 'printers'
+        ordering = ['-id']
+
+
+class Check(models.Model):
+
+    def files_directory_path(instance, filename):
+        filename = str(instance.order['key']) + '_' + str(instance.type) + '.pdf'
+        return 'avatar_img/{0}'.format(filename)
+
+    printer_id = models.ForeignKey(Printer, on_delete=models.CASCADE, 
+                                   verbose_name='Принтер')
+    type = models.CharField(max_length=10, verbose_name='Тип чека')
+    order = models.JSONField(verbose_name='Информация о заказе')
+    status = models.CharField(max_length=10, verbose_name='Статус чека')
+    pdf_file = models.FileField(upload_to=files_directory_path,
+                                blank=True, null=True, )
+
+    def __unicode__(self):
+        return str(self.id)
+
+    def __str__(self):
+        return str(self.id)
+
+    class Meta():
+        verbose_name = 'Чек'
+        verbose_name_plural = 'Чеки'
+        db_table = 'checks'
+        ordering = ['-id']
